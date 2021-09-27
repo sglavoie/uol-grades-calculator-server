719d91929cf83000a117b02a315a1d5c50a17388
Sébastien Lavoie <sgdlavoie@gmail.com>
2021-09-26 20:30:26 -0500

    WIP: Read content of file as JSON when uploading

--- frontend/src/components/Uploader/Uploader.tsx
+++ frontend/src/components/Uploader/Uploader.tsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
-import {useAppDispatch} from '../../hooks';
-import {setGrades, setGradesLoaded} from '../../features/grades/gradesSlice';
+import { useAppDispatch } from '../../hooks';
+import { setGrades, setGradesLoaded } from '../../features/grades/gradesSlice';
 import CONFIG from '../../config';
 
 const Uploader = (): JSX.Element => {
   const dispatch = useAppDispatch();
+
+  const [selectedFile, setSelectedFile] = useState({});
+  const [isFilePicked, setIsFilePicked] = useState(false);
+  const [fileContent, setFileContent] = useState({});
+
+  function setFileContentAsJSON(event: ProgressEvent<FileReader>) {
+    const json = JSON.parse(String(event?.target?.['result']) || '');
+    const jsonMinified = JSON.stringify(json);
+    setFileContent(jsonMinified);
+  }
+
+  const changeHandler = (event) => {
+    const file = event.target['files'][0];
+    setSelectedFile(file);
+    const fileReader = new FileReader();
+    fileReader.onload = setFileContentAsJSON;
+    fileReader.readAsText(file);
+    setIsFilePicked(true);
+  };
+
+  // TODO: this function should do the following:
+  // - Read the uploaded file as a string
+  // - Send the string as an URL parameter to the backend to instantiate a new Grades object and get the config in return
+  // - Should handle failure when the config isn't valid and set states accordingly (alert and re-display component)
+  // - Should handle successful response by calling the setGrades action so the uploader component isn't shown anymore
+  const handleFileUpload = () => {
+    if (!isFilePicked) return alert('Please select a file to upload.');
+    // uploadedFileIsValid()
+    console.log(selectedFile, typeof selectedFile);
+    console.log('uploadGradesFile:', isFilePicked, selectedFile);
+    console.log('fileContent:', fileContent);
+  };
 
   const loadDefaultGradesTemplate = async () => {
     try {
@@ -21,9 +53,20 @@ const Uploader = (): JSX.Element => {
 
   return (
     <div>
-      <span style={{color: 'red'}}>No grades found.</span>
+      <span style={{ color: 'red' }}>No grades found.</span>
       <div>
-        <button>Upload existing configuration file...</button>
+        <div>
+          <input
+            type="file"
+            name="file"
+            accept=".json"
+            onChange={changeHandler}
+          />
+          <div>
+            <button onClick={handleFileUpload}>Upload grades...</button>
+          </div>
+        </div>
+        <br />
       </div>
       <div>
         <button onClick={loadDefaultGradesTemplate}>

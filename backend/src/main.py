e10d7d71829a59ed405529a27ba332ffc64218dd
Sébastien Lavoie <sgdlavoie@gmail.com>
2021-10-19 10:28:32 -0500

    backend: Enable auto-reload with uvicorn by default
    As per the docs at
    https://www.uvicorn.org/settings/

--- backend/src/main.py
+++ backend/src/main.py
@@ -130,4 +130,4 @@ async def validate_config(json_str: Dict[Any, Any] = None):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)

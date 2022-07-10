3954e9e445e3637ad3eabea9a6e135271d7be619
Sébastien Lavoie <sgdlavoie@gmail.com>
2022-07-09 20:01:32 -0500

    Add Docker setup with docker-compose
    * The backend works, the frontend will need some updates which aren't
      going to happen now.

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,7 +1,71 @@
+const {
+  when,
+  whenDev,
+  whenProd,
+  whenTest,
+  ESLINT_MODES,
+  POSTCSS_MODES,
+} = require('@craco/craco');
+
 module.exports = {
-  style: {
-    postcss: {
-      plugins: [require('tailwindcss'), require('autoprefixer')],
+  reactScriptsVersion: 'react-scripts' /* (default value) */,
+  eslint: {
+    enable: true /* (default value) */,
+    mode: 'extends' /* (default value) */ || 'file',
+  },
+  babel: {
+    presets: [],
+    plugins: [],
+  },
+  typescript: {
+    enableTypeChecking: true /* (default value)  */,
+  },
+  webpack: {
+    alias: {},
+    plugins: {},
+  },
+  jest: {
+    babel: {
+      addPresets: true /* (default value) */,
+      addPlugins: true /* (default value) */,
     },
   },
+  plugins: [
+    {
+      plugin: {
+        overrideCracoConfig: ({
+          cracoConfig,
+          pluginOptions,
+          context: { env, paths },
+        }) => {
+          return cracoConfig;
+        },
+        overrideWebpackConfig: ({
+          webpackConfig,
+          cracoConfig,
+          pluginOptions,
+          context: { env, paths },
+        }) => {
+          return webpackConfig;
+        },
+        overrideDevServerConfig: ({
+          devServerConfig,
+          cracoConfig,
+          pluginOptions,
+          context: { env, paths, proxy, allowedHost },
+        }) => {
+          return devServerConfig;
+        },
+        overrideJestConfig: ({
+          jestConfig,
+          cracoConfig,
+          pluginOptions,
+          context: { env, paths, resolve, rootDir },
+        }) => {
+          return jestConfig;
+        },
+      },
+      options: {},
+    },
+  ],
 };
